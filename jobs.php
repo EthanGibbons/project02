3f0d6279c5f5f25794f208103e7d1a60e4239f64
MysteriousRaise <EthanGibbons@hotmail.com>
2025-05-23 06:25:40 +1000

    Jobs_ databases created

--- jobs.php
+++ jobs.php
@@ -23,7 +23,7 @@
         <?php
             require_once("settings.php");
 
-            $query = "SELECT * FROM jobs";
+            $query = "SELECT * FROM jobs_";
             $result = mysqli_query($conn, $query);
             if ($result) {
                 while ($row = mysqli_fetch_assoc($result)) {
@@ -87,85 +87,6 @@
 
 
             ?>
-        <!--Listing for Network Administrator--> 
-        <section>
-            <h3>Network Administrator</h3>
-            <p>Job Reference: 102</p>
-            <p>Salary: $105,120 per year</p>
-            <p>Administrator involved in the implementation of company networking hardware and software.</p>
-            <p>This position will report to the Head of the Internal IT Department</p>
-            <h4>This position Involves</h4>
-            <ol>
-                <li>Implementing and initializing networking devices.</li>
-                <li>Connecting all userdevices to the company network.</li>
-                <li>Reporting and resolving networking issues encoutered.</li>
-            </ol>
-            
-            <br>
-
-            <section>
-                <h4>Essential Skills</h4>
-                <ul>
-                    <li>Bachelor of IT or equivelent qualification</li>
-                    <li>3 or more years work in networking administration</li>
-                    <li>Strong understanding of networking structure and security</li>
-                </ul>
-            </section>
-
-            <br>
-
-            <section>
-                <h4>Preferable Skills</h4>
-                <ul>
-                    <li>Familiaity will common network interfaces and devices</li>
-                    <li>Experience with setting up physical networks</li>
-                </ul>
-            </section>
-            <br>
-            <a href="apply.html" title="Go to application page"><strong>Apply Now!</strong></a>
-        </section>
-
-        <br>
-        <br>
-        
-        <!--Job listing for Cybersecurity Specialist-->
-        <section>
-            <h3>Cybersecurity Specialist</h3>
-            <p>Job Reference: 101</p>
-            <p>Salary: $135,970 per year</p>
-            <p>The primary specialist in charge of implementing and upkeeping software
-                 and hardware solutions for involved clients and businesses.</p>
-            <p>This position will report to the Head of the Development</p>
-            <h4>This position Involves</h4>
-            <ol>
-                <li>Upkeeping the security aspects of the client network.</li>
-                <li>Ensuring the most optimanal cyber security configuations and softwares are running all devices.</li>
-                <LI>Reporting and resolving compromisations.</LI>
-            </ol>
-            
-            <br>
-
-            <section>
-                <h4>Essential Skills</h4>
-                <ul>
-                    <li>Bachelor of Cybersecurity or equivelent qualification</li>
-                    <li>3 or more years work in Cybersecurity</li>
-                    <li>Strong understanding of networking structure and security</li>
-                </ul>
-            </section>
-
-            <br>
-
-            <section>
-                <h4>Preferable Skills</h4>
-                <ul>
-                    <li>Qualification/s in networking</li>
-                    <li>Experience with common antivirus programs</li>
-                </ul>
-            </section>
-            <br>
-            <a href="apply.html" title="Go to application page" ><strong>Apply Now!</strong></a>
-        </section>
 
         <br>
 
